0f17e07164d83b63adde83435cf392d8f31d7bbc
erha riaz <erharandhawa@gmail.com>
2024-03-20 21:32:55 +0500

    change commited

--- Lab_06/Task_02.cpp
+++ Lab_06/Task_02.cpp
@@ -29,6 +29,42 @@ public:
   }
 };
 
+class Library {
+private:
+  vector<Book> books;
+
+public:
+  // Method to add a book to the library
+  void addBook(const Book& book) {
+    books.push_back(book);
+    cout << "Book added successfully!\n";
+  }
+
+  // Method to remove a book from the library by title (using std::find)
+  bool removeBook(const string& title) {
+    auto it = find(books.begin(), books.end(), Book(title, "", 0)); // Search by title only
+    if (it != books.end()) {
+      books.erase(it);
+      cout << "Book removed successfully!\n";
+      return true;
+    } else {
+      cout << "Book not found!\n";
+      return false;
+    }
+  }
+
+  // Method to list all books in the library
+  void listBooks() const {
+    if (books.empty()) {
+      cout << "Library is empty.\n";
+      return;
+    }
+    cout << "Books in the library:\n";
+    for (const auto& book : books) {
+      book.display();
+    }
+  }
+};
 
 
 int main() 
