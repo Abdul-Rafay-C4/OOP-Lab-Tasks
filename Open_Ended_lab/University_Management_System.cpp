7a413157975b6b9c3fda0dfdfc67807529df413b
erha riaz <erharandhawa@gmail.com>
2024-03-21 13:58:19 +0500

    changes added

--- Open_Ended_lab/University_Management_System.cpp
+++ Open_Ended_lab/University_Management_System.cpp
@@ -22,6 +22,44 @@ public:
     void dropCourse(Course*course);
     void viewCourses();
     string getStudentID() const {return studentID;}
+
+void enrollCourse(Course*course)
+{
+    if(course->studentsEnrolled.size() < course-> maxCapacity)
+    {
+        coursesEnrolled.push_back(course);
+        course -> addStudent(this);
+        cout << "Enrolled in " << course -> courseName << "." << endl;
+    }
+    else
+    {
+        cout << "Cannot enroll in " << course -> courseName << ". Course is full." << endl;
+    }
+}
+
+void dropCourse(Course* course)
+{
+    auto it = find(coursesEnrolled.begin(), coursesEnrolled.end(), course);
+    if(it != coursesEnrolled.end())
+    {
+        coursesEnrolled.erase(it);
+        course -> removeStudent(this);
+        cout << "Dropped " << course -> courseName << "." << endl;
+    }
+    else
+    {
+        cout << "Not enrolled in " << course -> courseName << "." << endl;
+    }
+}
+
+void viewCourses()
+    {
+    cout << "Enrolled Courses:" << endl;
+    for(auto course : coursesEnrolled)
+    {
+        cout << course -> courseName << endl;
+    }
+}
 };
 
 class Teacher
@@ -38,6 +76,37 @@ public:
     void assignCourse(Course* course);
     void removeCourse(Course* course);
     void viewCourses();
+
+    void assignCourse(Course* course)
+{
+    coursesTaught.push_back(course);
+    course -> teacher = this;
+    cout << "Assigned " << course -> courseName << " to " << name << "." << endl;
+}
+
+void removeCourse(Course* course)
+{
+    auto it = find(coursesTaught.begin(), coursesTaught.end(), course);
+    if (it != coursesTaught.end())
+    {
+        coursesTaught.erase(it);
+        course -> teacher = nullptr;
+        cout << "Removed " << course -> courseName << " from " << name << "." << endl;
+    }
+    else
+    {
+        cout << "Not teaching " << course -> courseName << "." << endl;
+    }
+}
+
+void viewCourses()
+    {
+    cout << "Courses Taught:" << endl;
+    for (auto course : coursesTaught)
+    {
+        cout << course -> courseName << endl;
+    }
+}
 };
 
 class Course
@@ -82,71 +151,4 @@ int main()
 
 }
 
-void Student::enrollCourse(Course*course)
-{
-    if(course->studentsEnrolled.size() < course-> maxCapacity)
-    {
-        coursesEnrolled.push_back(course);
-        course -> addStudent(this);
-        cout << "Enrolled in " << course -> courseName << "." << endl;
-    }
-    else
-    {
-        cout << "Cannot enroll in " << course -> courseName << ". Course is full." << endl;
-    }
-}
 
-void Student::dropCourse(Course* course)
-{
-    auto it = find(coursesEnrolled.begin(), coursesEnrolled.end(), course);
-    if(it != coursesEnrolled.end())
-    {
-        coursesEnrolled.erase(it);
-        course -> removeStudent(this);
-        cout << "Dropped " << course -> courseName << "." << endl;
-    }
-    else
-    {
-        cout << "Not enrolled in " << course -> courseName << "." << endl;
-    }
-}
-
-void Student::viewCourses()
-    {
-    cout << "Enrolled Courses:" << endl;
-    for(auto course : coursesEnrolled)
-    {
-        cout << course -> courseName << endl;
-    }
-}
-
-void Teacher::assignCourse(Course* course)
-{
-    coursesTaught.push_back(course);
-    course -> teacher = this;
-    cout << "Assigned " << course -> courseName << " to " << name << "." << endl;
-}
-
-void Teacher::removeCourse(Course* course)
-{
-    auto it = find(coursesTaught.begin(), coursesTaught.end(), course);
-    if (it != coursesTaught.end())
-    {
-        coursesTaught.erase(it);
-        course -> teacher = nullptr;
-        cout << "Removed " << course -> courseName << " from " << name << "." << endl;
-    }
-    else
-    {
-        cout << "Not teaching " << course -> courseName << "." << endl;
-    }
-}
-
-void Teacher::viewCourses()
-    {
-    cout << "Courses Taught:" << endl;
-    for (auto course : coursesTaught)
-    {
-        cout << course -> courseName << endl;
-    }
-}
