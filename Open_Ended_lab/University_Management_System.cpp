7c68bfb4a6fc7af7accca6bfc1eaf4896456a303
Abdul Rafay <rafay0820188@gmail.com>
2024-03-21 14:56:54 +0500

    fixing errors by making the methodes define outside the class

--- Open_Ended_lab/University_Management_System.cpp
+++ Open_Ended_lab/University_Management_System.cpp
@@ -19,44 +19,9 @@ class Student
 
     public:
         Student(string studentID, string name, string email): studentID(studentID), name(name), email(email) {}
-
-        void enrollCourse(Course*course)
-        {
-            if(course ->studentsEnrolled.size() < course ->maxCapacity)
-            {
-                coursesEnrolled.push_back(course);
-                course -> addStudent(this);
-                cout << "Enrolled in " << course -> courseName << "." << endl;
-            }
-            else
-            {
-                cout << "Cannot enroll in " << course -> courseName << ". Course is full." << endl;
-            }
-        }
-
-        void dropCourse(Course* course)
-        {
-            auto it = find(coursesEnrolled.begin(), coursesEnrolled.end(), course);
-            if(it != coursesEnrolled.end())
-            {
-                coursesEnrolled.erase(it);
-                course -> removeStudent(this);
-                cout << "Dropped " << course -> courseName << "." << endl;
-            }
-            else
-            {
-                cout << "Not enrolled in " << course -> courseName << "." << endl;
-            }
-        }
-
-        void viewCourses()
-            {
-            cout << "Enrolled Courses:" << endl;
-            for(auto course : coursesEnrolled)
-            {
-                cout << course -> courseName << endl;
-            }
-        }
+        void enrollCourse(Course*course);
+        void dropCourse(Course* course);
+        void viewCourses();
         friend class Course;
 };
 
@@ -71,36 +36,9 @@ class Teacher
     public:
         Teacher(string teacherID, string name, string email) : teacherID(teacherID), name(name), email(email) {}
 
-        void assignCourse(Course* course)
-        {
-            coursesTaught.push_back(course);
-            course -> teacher = this;
-            cout << "Assigned " << course -> courseName << " to " << name << "." << endl;
-        }
-
-        void removeCourse(Course* course)
-        {
-            auto it = find(coursesTaught.begin(), coursesTaught.end(), course);
-            if (it != coursesTaught.end())
-            {
-                coursesTaught.erase(it);
-                course -> teacher = nullptr;
-                cout << "Removed " << course -> courseName << " from " << name << "." << endl;
-            }
-            else
-            {
-                cout << "Not teaching " << course -> courseName << "." << endl;
-            }
-        }
-
-        void viewCourses()
-        {
-            cout << "Courses Taught:" << endl;
-            for (auto course : coursesTaught)
-            {
-                cout << course -> courseName << endl;
-            }
-        }
+        void assignCourse(Course* course);
+        void removeCourse(Course* course);
+        void viewCourses();
         friend class Course;
 };
 
@@ -147,4 +85,75 @@ class Course
 int main()
 {
 
+}
+
+//Studnet Class Methods
+void Student::enrollCourse(Course*course)
+{
+    if(course -> studentsEnrolled.size() < course -> maxCapacity)
+    {
+        coursesEnrolled.push_back(course);
+        course -> addStudent(this);
+        cout << "Enrolled in " << course -> courseName << "." << endl;
+    }
+    else
+    {
+        cout << "Cannot enroll in " << course -> courseName << ". Course is full." << endl;
+    }
+}
+
+void Student::dropCourse(Course* course)
+{
+    auto it = find(coursesEnrolled.begin(), coursesEnrolled.end(), course);
+    if(it != coursesEnrolled.end())
+    {
+        coursesEnrolled.erase(it);
+        course -> removeStudent(this);
+        cout << "Dropped " << course -> courseName << "." << endl;
+    }
+    else
+    {
+        cout << "Not enrolled in " << course -> courseName << "." << endl;
+    }
+}
+
+void Student::viewCourses()
+{
+    cout << "Enrolled Courses:" << endl;
+    for(auto course : coursesEnrolled)
+    {
+        cout << course -> courseName << endl;
+    }
+}
+
+//Teacher Class Methods
+void Teacher::assignCourse(Course* course)
+{
+    coursesTaught.push_back(course);
+    course -> teacher = this;
+    cout << "Assigned " << course -> courseName << " to " << name << "." << endl;
+}
+
+void Teacher::removeCourse(Course* course)
+{
+    auto it = find(coursesTaught.begin(), coursesTaught.end(), course);
+    if(it != coursesTaught.end())
+    {
+        coursesTaught.erase(it);
+        course -> teacher = nullptr;
+        cout << "Removed " << course -> courseName << " from " << name << "." << endl;
+    }
+    else
+    {
+        cout << "Not teaching " << course -> courseName << "." << endl;
+    }
+}
+
+void Teacher::viewCourses()
+{
+    cout << "Courses Taught:" << endl;
+    for(auto course : coursesTaught)
+    {
+        cout << course -> courseName << endl;
+    }
 }
