4f36631963ee1ce9f74b3091ff9941a9c0d91a7d
Abdul Rafay <rafay0820188@gmail.com>
2024-03-21 11:12:24 +0500

    skeleton of the code

---
+++ Open_Ended_lab/University_Management_System.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Student 
+{
+};
+
+class Teacher
+{
+};
+
+class Course
+{
+};
+
+int main()
+{
+}
